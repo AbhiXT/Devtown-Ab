14bd91a28585e7123db361a92e423a3f5c220f2e
Abhishek Sharma <106692721+AbhiXT@users.noreply.github.com>
2022-07-07 15:53:59 +0530

    Initial commit

---
+++ src/Components/Notes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Hii,Its Abhishek Sharma </h1>
+      <p>
+        I am a actor playing a role of an engineer until i become an engineer
+        .Learner by Birth. Carrying a Never giving up attitude , Believes that
+        Every problem comes with a solution . Looking for Some opportunities to
+        enhance my Carrier by using my skills in a Proper way.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
